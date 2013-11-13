61a873d30c73ff2aabd2e63411e27ea209bf070d
Martin Fitzpatrick <mfitzp@abl.es>
2013-11-13 23:47:07 +0000

    Google rich snippets

--- pelicanconf.py
+++ pelicanconf.py
@@ -41,7 +41,7 @@ LINKS =  (
 SOCIAL = (
           ('RSS', 'rss', '%s/%s' % (FEED_DOMAIN, FEED_ATOM) ),
 #          ('Facebook', 'facebook', 'https://facebook.com/research.abl.es'),
-          ('Google+', 'google-plus', 'https://plus.google.com/115539678583643563408/'),
+          ('Google+', 'google-plus', 'https://plus.google.com/115539678583643563408/?rel=author'),
           ('ORCID', 'orcid', 'http://orcid.org/0000-0002-0695-1988'),
           ('Twitter', 'twitter', 'https://twitter.com/mfitzp'),
           ('ResearchGate', 'researchgate', 'https://www.researchgate.net/profile/Martin_Fitzpatrick/'),
