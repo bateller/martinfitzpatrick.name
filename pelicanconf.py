a0e718031e56a845d99bf59086a8d4c76fdece07
Martin Fitzpatrick <mfitzp@abl.es>
2013-11-15 00:20:21 +0000

    Add LinkedIn

--- pelicanconf.py
+++ pelicanconf.py
@@ -42,6 +42,7 @@ SOCIAL = (
           ('RSS', 'rss', '%s/%s' % (FEED_DOMAIN, FEED_ATOM) ),
 #          ('Facebook', 'facebook', 'https://facebook.com/research.abl.es'),
           ('Google+', 'google-plus', 'https://plus.google.com/115539678583643563408/?rel=author'),
+          ('LinkedIn', 'linkedin', 'uk.linkedin.com/in/martinfitzp'),
           ('ORCID', 'orcid', 'http://orcid.org/0000-0002-0695-1988'),
           ('Twitter', 'twitter', 'https://twitter.com/mfitzp'),
           ('ResearchGate', 'researchgate', 'https://www.researchgate.net/profile/Martin_Fitzpatrick/'),
