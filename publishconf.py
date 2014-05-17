a61fe1a83cddf3aef2d5884064d7f6181bcfbd83
Martin Fitzpatrick <mfitzp@abl.es>
2014-05-17 18:28:48 +0100

    Fix up urls

--- publishconf.py
+++ publishconf.py
@@ -19,7 +19,16 @@ RELATIVE_URLS = False
 #GOOGLE_ANALYTICS = ""
 
 ARTICLE_URL = 'article/{slug}/'
+ARTICLE_SAVE_AS = 'article/{slug}/index.html'
+
 PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
 AUTHOR_URL = 'author/{slug}/'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+
 CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+
 TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
